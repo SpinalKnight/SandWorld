d712e3c581e224dfc630950139a02f0e7f95d364
SpinalKnight <102822364+SpinalKnight@users.noreply.github.com>
2022-05-08 23:39:14 -0600

    0.1
    Added basic parallax background along with basic player movement and.
    
    Future:
    -fix parallax y position changing bug
    -add character model
    -add enemies and actual level design
    -add multiple levels

---
+++ Parallax/Assets/Scripts/Respawn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform PlayerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
+
